fbedd4600df4c5621c86990d61d11612e5c55ee4
6203 <rlaekdlsld>
2022-06-03 13:41:55 +0900

    주석 추가했습니다.

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * 액티비티가 실행 될 떄 호출되는 함수입니다.
+         * 인텐트를 DeloyedMessageService 객체로 넘겨줍니다.
+         * @param view 뷰 클래스의 객체를 인자로 넘겨받습니다.
+         * @Warning 주의사항 없습니다.
+         * @sww DeloyedMessageService
+         */
     }
 }
